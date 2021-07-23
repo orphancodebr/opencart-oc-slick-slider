9c458309feba6c9177a6b6fe65611b654485ba4d
Jano Sousa <sollykma@gmail.com>
2021-07-23 12:50:42 -0300

    Enviado todos os arquivos

---
+++ upload/admin/language/en-gb/extension/module/oc_slick.php
@@ -0,0 +1,21 @@
+<?php
+// Heading
+$_['heading_title']    = 'OC-slick';
+
+// Text
+$_['text_extension']   = 'Extensions';
+$_['text_success']     = 'Success: You have modified OC-slick module!';
+$_['text_edit']        = 'Edit OC-slick Module';
+
+// Entry
+$_['entry_name']       = 'Module Name';
+$_['entry_banner']     = 'Banner';
+$_['entry_width']      = 'Width';
+$_['entry_height']     = 'Height';
+$_['entry_status']     = 'Status';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify OC-slick module!';
+$_['error_name']       = 'Module Name must be between 3 and 64 characters!';
+$_['error_width']      = 'Width required!';
+$_['error_height']     = 'Height required!';
---
+++ upload/admin/language/pt-br/extension/module/oc_slick.php
@@ -0,0 +1,21 @@
+<?php
+// Heading
+$_['heading_title']    = 'OC-slick';
+
+// Text
+$_['text_extension']   = 'Extensões';
+$_['text_success']     = 'OC-slick modificado com sucesso!';
+$_['text_edit']        = 'Editando OC-slick';
+
+// Entry
+$_['entry_name']       = 'Título do módulo';
+$_['entry_banner']     = 'Banner';
+$_['entry_width']      = 'Largura';
+$_['entry_height']     = 'Altura';
+$_['entry_status']     = 'Situação';
+
+// Error
+$_['error_permission'] = 'Atenção: Você não tem permissão para modificar a extensão OC-slick!';
+$_['error_name']       = 'Título do módulo deve ter entre 3 e 64 caracteres!';
+$_['error_width']      = 'Largura é obrigatória!';
+$_['error_height']     = 'Altura é obrigatória!';
